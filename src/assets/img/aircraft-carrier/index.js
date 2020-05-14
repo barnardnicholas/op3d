5b3c418c96627abe336ef3ef7d51db8004b6062c
barnardnicholas <barnard.nicholas@googlemail.com>
2020-05-14 16:24:54 +0100

    add extra layer to aircraft carrier

--- src/assets/img/aircraft-carrier/index.js
+++ src/assets/img/aircraft-carrier/index.js
@@ -5,11 +5,12 @@ import pic01 from "./01.png";
 import pic02 from "./02.png";
 import pic03 from "./03.png";
 import pic04 from "./04.png";
+import pic05 from "./05.png";
 
 const aircraftCarrier = {
   name: "Aircraft Carrier",
   path: "/aircraft-carrier",
-  img: [pic00, pic01, pic02, pic03, pic04],
+  img: [pic00, pic01, pic02, pic03, pic04, pic05],
   picMaster: picMaster,
   thumb: thumb,
   dimX: 1280,
